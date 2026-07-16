934e0e9aa189be6bbdeb186fdfa4c66f88c66271
Tanmaykurund <7tanmaykurund@gmail.com>
2026-07-17 02:35:44 +0530

    fixes

--- lib/card.dart
+++ lib/card.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 class CustomCard extends StatelessWidget {
   final String title;
-  final int value;
+  final num value;
   final String unit;
   final double width;
   final IconData icon;
   final Color iconColor;
+
   const CustomCard({
     super.key,
     required this.title,
@@ -19,6 +20,8 @@ class CustomCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final displayValue = value is double ? value.toStringAsFixed(1) : value.toString();
+
     return Container(
       margin: const EdgeInsets.all(6),
       width: width,
@@ -64,7 +67,9 @@ class CustomCard extends StatelessWidget {
               Container(
                 padding: const EdgeInsets.all(8),
                 child: Text(
-                  '$value $unit',
+                  '$displayValue $unit',
+                  overflow: TextOverflow.ellipsis,
+                  maxLines: 1,
                   style: TextStyle(
                     fontSize: 14,
                     fontWeight: FontWeight.bold,
